1f77af5dc4c824b26dcc9beff2457cdd17338566
podestal <luisrodriguezpodesta@gmail.com>
2024-07-16 20:38:04 -0500

    connected to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react"
+import axios from "axios"
+
 const App = () => {
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/dishes/')
+    .then(res => console.log(res.data))
+  }, [])
 
   return (
     <h1 className="text-8xl font-bold">Hello World</h1>
