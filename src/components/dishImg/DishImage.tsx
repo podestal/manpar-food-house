d127119648bdf34b10579fac4bf6439de8ee3712
podestal <luisrodriguezpodesta@gmail.com>
2024-08-21 11:43:58 -0400

    fixing overflow errors

--- src/components/dishImg/DishImage.tsx
+++ src/components/dishImg/DishImage.tsx
@@ -18,8 +18,8 @@ const DishImage = ({ dishId, alt }: Props) => {
     if (isSuccess)
 
   return (
-    <div className="flex flex-col justify-center items-center sticky z-30">
-        <img className="sticky z-30 object-cover my-10 max-md:w-full h-[240px] lg:w-[360px] lg:h-[220px] rounded-3xl" src={img[0]?.image} alt={alt}  />
+    <div className="flex flex-col justify-center items-center sticky z-30 overflow-hidden">
+        <img className="sticky z-30 object-cover my-10 max-md:w-[280px] h-[200px] lg:h[260px] lg:w-[360px] rounded-3xl" src={img[0]?.image} alt={alt}  />
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,4 @@ export default defineConfig({
       'api': 'https://manpar-api.vercel.app/api'
     }
     },
-})
+})  
